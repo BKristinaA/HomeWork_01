257712724829dc1bc73bdf8262ca0bae9be8859b
Christina <bihtya@mail.ru>
2023-01-20 14:43:58 +0300

    Первая задача

---
+++ Exp01/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int a = 2;
+int b = 20;
+int max = 0;
+int min = 0;
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("Max " + max);
+Console.WriteLine("Min " + min);
