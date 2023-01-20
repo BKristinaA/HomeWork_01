4a3964739aca0f89a9675d2882ccda20c327b334
Christina <bihtya@mail.ru>
2023-01-20 18:17:31 +0300

    Написали код

--- Exp02/Program.cs
+++ Exp02/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+int[] array = {44, 5 ,78};
+
+
+int index = 0;
+int max = 0;
+
+while (index < array.Length)
+{
+  if (array[index] > max) 
+    {
+    max = array[index];
+    }
+index = index + 1;
+}
+
+Console.WriteLine("max " + max);
